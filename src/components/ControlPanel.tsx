fe82d64160942adbc1022a5454d18fbff8f8c987
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2026-02-22 03:07:30 +0700

    update chevron

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -616,7 +616,7 @@ export const ControlPanel = () => {
           
           {/* Chevron Indicator for Minimize/Expand */}
           <div className="text-black-400 bg-black/10 rounded p-0.5">
-             {isTerrainControlsOpen ? <ChevronDown size={16} /> : <ChevronUp size={16} />}
+             {isTerrainControlsOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
           </div>
         </div>
 
